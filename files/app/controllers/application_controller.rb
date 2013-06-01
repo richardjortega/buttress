c805d8bfa5d765da2f95dfa00bf877f1a6678291
JDStraughan <jdstraughan@gmail.com>
2013-06-01 17:25:00 -0500

    Remove unneeded method now handled by rolify

--- files/app/controllers/application_controller.rb
+++ files/app/controllers/application_controller.rb
@@ -11,11 +11,6 @@ class ApplicationController < ActionController::Base
     @authed_user = current_user
   end
 
-  # A before filter for authing someone as an admin
-  def authenticate_admin!
-    redirect_to root_path unless current_user.present? && current_user.is_admin?
-  end
-
   # Rescue from an access denied thrown by cancan
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
