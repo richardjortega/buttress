e4be1b0ae0b6a956fbca46ceb7a9392db70be413
JDStraughan <jdstraughan@gmail.com>
2013-06-01 19:52:59 -0500

    Update seeds.rb to include first/last names

--- files/db/seeds.rb
+++ files/db/seeds.rb
@@ -1,12 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-# Create admin user
+
 user = User.create(
+  first_name: 'Admin',
+  last_name: 'User',
   email: 'admin@example.org', 
   password: 'letmein123', 
   password_confirmation: 'letmein123'
