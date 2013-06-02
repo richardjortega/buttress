1423138083e6d6ea50bb52cdac62056c5157ddd4
JDStraughan <jdstraughan@gmail.com>
2013-06-01 19:52:35 -0500

    Add user model with search method and first/last name accessors

---
+++ files/app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ActiveRecord::Base
+  rolify
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
+  # attr_accessible :title, :body
+  
+  # Simple search method
+  def self.search(query)
+    if query.present?
+      search(query)
+    else
+      scoped
+    end
+  end
+  
+end
